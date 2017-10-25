5d5fb8a51dd2d0d21983e999bdc6494aab30ea0b
luisaGonzales <luisagc711@gmail.com>
2017-10-24 22:21:21 -0500

    Estructura app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'redux-zero/react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const App = ({playlist, actualSong, play}) => {
+  return (
+    <div></div>
+  );
 }
 
-export default App;
+const mapToProps = ({playlist, actualSong, play}) => ({playlist, actualSong, play});
+export default connect (mapToProps)(App);
